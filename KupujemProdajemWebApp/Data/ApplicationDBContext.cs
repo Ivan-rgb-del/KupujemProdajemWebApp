69df95f8eb7660d554b03c3eeae9eba15cff13ea
Ivan Knezevic <knezevicivan500@gmail.com>
2024-10-28 22:37:27 +0100

    Installed EntityCore and created DbContext

---
+++ KupujemProdajemWebApp/Data/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using KupujemProdajemWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KupujemProdajemWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Advertisement> Advertisements { get; set; }
+        public DbSet<AdvertisementCategory> AdvertisementCategories { get; set; }
+        public DbSet<AdvertisementGroup> AdvertisementGroups { get; set; }
+        public DbSet<Favorite> Favorites { get; set; }
+    }
+}
