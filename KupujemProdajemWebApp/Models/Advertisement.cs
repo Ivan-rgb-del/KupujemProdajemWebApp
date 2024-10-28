2f36ac02ed7c8ccc39cbc9e9311a1dd08b286c14
Ivan Knezevic <knezevicivan500@gmail.com>
2024-10-28 20:50:57 +0100

    Created Advertisement Model

---
+++ KupujemProdajemWebApp/Models/Advertisement.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KupujemProdajemWebApp.Models
+{
+    public class Advertisement
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+        public bool IsFixedPrice { get; set; }
+        public bool IsReplacement { get; set; }
+        public string Description { get; set; }
+        public string ImageURL { get; set; }
+        public int Likes { get; set; }
+        public int Viewers { get; set; }
+        public DateOnly CreatedOn { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
