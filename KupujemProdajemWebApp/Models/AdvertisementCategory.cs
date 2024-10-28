885772c2141b0955cb7ab8412ed0210ee80f113d
Ivan Knezevic <knezevicivan500@gmail.com>
2024-10-28 21:45:42 +0100

    Added Category model

---
+++ KupujemProdajemWebApp/Models/AdvertisementCategory.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KupujemProdajemWebApp.Models
+{
+    public class AdvertisementCategory
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
