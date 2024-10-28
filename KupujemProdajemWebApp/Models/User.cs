658405915355dd3056de487e7592cab509d9373b
Ivan Knezevic <knezevicivan500@gmail.com>
2024-10-28 20:55:03 +0100

    Created User model

---
+++ KupujemProdajemWebApp/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace KupujemProdajemWebApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string City { get; set; }
+        public string Addrress { get; set; }
+        public int PhoneNumber { get; set; }
+    }
+}
