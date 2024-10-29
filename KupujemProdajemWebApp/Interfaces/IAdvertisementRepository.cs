cae19b66388a173d3f9030269d88603aa2f665b2
Ivan Knezevic <knezevicivan500@gmail.com>
2024-10-29 20:36:00 +0100

    Created Interface for Advertisement

---
+++ KupujemProdajemWebApp/Interfaces/IAdvertisementRepository.cs
@@ -0,0 +1,14 @@
+﻿using KupujemProdajemWebApp.Models;
+
+namespace KupujemProdajemWebApp.Interfaces
+{
+    public interface IAdvertisementRepository
+    {
+        Task<IEnumerable<Advertisement>> GetAll();
+        Task<Advertisement> GetByIdAsync(int id);
+        bool Add(Advertisement advertisement);
+        bool Update(Advertisement advertisement);
+        bool Delete(Advertisement advertisement);
+        bool Save();
+    }
+}
