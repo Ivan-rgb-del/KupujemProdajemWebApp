750739d237361fc341210e9b3f31474f913f79e8
Ivan Knezevic <knezevicivan500@gmail.com>
2024-10-29 20:47:28 +0100

    Created repository for Advertisement

---
+++ KupujemProdajemWebApp/Repository/AdvertisementRepository.cs
@@ -0,0 +1,46 @@
+﻿using KupujemProdajemWebApp.Data;
+using KupujemProdajemWebApp.Interfaces;
+using KupujemProdajemWebApp.Models;
+
+namespace KupujemProdajemWebApp.Repository
+{
+    public class AdvertisementRepository : IAdvertisementRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdvertisementRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Add(Advertisement advertisement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Advertisement advertisement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Advertisement>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Advertisement> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Advertisement advertisement)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
