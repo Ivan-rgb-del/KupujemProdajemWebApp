8326d5be1c37c3c5244df273c2febc68741d0434
Ivan Knezevic <knezevicivan500@gmail.com>
2024-10-29 14:30:17 +0100

    Added view for home page

---
+++ KupujemProdajemWebApp/Controllers/AdvertisementController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KupujemProdajemWebApp.Controllers
+{
+    public class AdvertisementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
