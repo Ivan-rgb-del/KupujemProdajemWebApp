15ded493857a12a73f3d2834be44ed2b1e869d20
Ivan Knezevic <knezevicivan500@gmail.com>
2024-10-29 21:06:32 +0100

    Add Services

--- KupujemProdajemWebApp/Program.cs
+++ KupujemProdajemWebApp/Program.cs
@@ -1,4 +1,5 @@
 using KupujemProdajemWebApp.Data;
+using KupujemProdajemWebApp.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddScoped<IAdvertisementRepository, IAdvertisementRepository>();
 
 var app = builder.Build();
 
