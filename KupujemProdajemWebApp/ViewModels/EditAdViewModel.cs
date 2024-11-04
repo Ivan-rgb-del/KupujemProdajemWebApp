eae2e720a5ac84005aba415f222d8818a338e32b
Ivan Knezevic <knezevicivan500@gmail.com>
2024-11-04 22:16:27 +0100

    Created Edit View Model for advertisement

---
+++ KupujemProdajemWebApp/ViewModels/EditAdViewModel.cs
@@ -0,0 +1,23 @@
+﻿using KupujemProdajemWebApp.Data.Enum;
+using KupujemProdajemWebApp.Models;
+
+namespace KupujemProdajemWebApp.ViewModels
+{
+    public class EditAdViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+        public bool IsFixedPrice { get; set; }
+        public bool IsReplacement { get; set; }
+        public string Description { get; set; }
+        public IFormFile ImageURL { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public bool IsActive { get; set; }
+        public AdvertisementCondition AdvertisementCondition { get; set; }
+        public DeliveryType DeliveryType { get; set; }
+        public int AdvertisementCategoryId { get; set; }
+        public int AdvertisementGroupId { get; set; }
+        public Address Address { get; set; }
+    }
+}
