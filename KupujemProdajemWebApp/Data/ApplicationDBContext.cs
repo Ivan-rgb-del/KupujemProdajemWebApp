9d2956999a5a38a3d6e548665a3e245240e742ba
Ivan Knezevic <knezevicivan500@gmail.com>
2024-11-05 22:23:44 +0100

    Setup int for user Id

--- KupujemProdajemWebApp/Data/ApplicationDBContext.cs
+++ KupujemProdajemWebApp/Data/ApplicationDBContext.cs
@@ -1,11 +1,12 @@
 ﻿using KupujemProdajemWebApp.Data.Enum;
 using KupujemProdajemWebApp.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace KupujemProdajemWebApp.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<User>
+    public class ApplicationDbContext : IdentityDbContext<User, IdentityRole<int>, int>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
--- KupujemProdajemWebApp/Models/User.cs
+++ KupujemProdajemWebApp/Models/User.cs
@@ -3,12 +3,8 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace KupujemProdajemWebApp.Models
 {
-    public class User : IdentityUser
+    public class User : IdentityUser<int>
     {
-        public int Id { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public int PhoneNumber { get; set; }
         public ICollection<Advertisement> Advertisements { get; set; } = new List<Advertisement>();
         public ICollection<Favorite> Favorites { get; set; } = new List<Favorite>();
         [ForeignKey("Address")]
