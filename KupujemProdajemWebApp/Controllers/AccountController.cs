1f852e401cd0dc051d6c13628dc7841a33cedd90
Ivan Knezevic <knezevicivan500@gmail.com>
2024-11-06 12:08:24 +0100

    Created RegosterViewModel and AccountController

---
+++ KupujemProdajemWebApp/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using KupujemProdajemWebApp.Data;
+using KupujemProdajemWebApp.Models;
+using KupujemProdajemWebApp.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KupujemProdajemWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager, ApplicationDbContext dbContext)
+        {
+            _context = dbContext;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Register()
+        {
+            var response = new RegisterViewModel();
+            return View(response);
+        }
+    }
+}
