f2682156fc3500ab4efd3b173d1e549d7e6241de
Ivan Knezevic <knezevicivan500@gmail.com>
2024-11-11 19:53:11 +0100

    Created method for getting all user ads

---
+++ KupujemProdajemWebApp/Repository/DashboardRepository.cs
@@ -0,0 +1,26 @@
+﻿using KupujemProdajemWebApp.Data;
+using KupujemProdajemWebApp.Interfaces;
+using KupujemProdajemWebApp.Models;
+
+namespace KupujemProdajemWebApp.Repository
+{
+    public class DashboardRepository : IDashboardRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public DashboardRepository(ApplicationDbContext context, IHttpContextAccessor httpContextAccessor)
+        {
+            _context = context;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<List<Advertisement>> GetAllUserAds()
+        {
+            var curUser = _httpContextAccessor.HttpContext?.User;
+            var userAds = _context.Advertisements.Where(a => a.UserId == curUser.ToString());
+
+            return userAds.ToList();
+        }
+    }
+}
