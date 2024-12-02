fb690234cb78deedd27341f1063afb9574b8cb80
Ivan Knezevic <knezevicivan500@gmail.com>
2024-12-02 12:27:26 +0100

    Created RegisterService

--- frontend/src/services/RegisterService.js
+++ frontend/src/services/RegisterService.js
@@ -1,15 +1,25 @@
 import React from 'react';
 
-export const registerNewUser = async () => {
+export const registerNewUser = async (userData) => {
     try {
-        const response = await fetch('https://localhost:7084/api/register');
+        const response = await fetch('https://localhost:7084/api/accountapi/register', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(userData)
+        });
+
         if (!response.ok) {
-            throw new Error("Failed to fetch ads!");
+            const errorData = await response.json();
+            throw new Error(errorData.message || "Registration failed!");
         }
-        return await response.json();
+
+        const data = await response.json();
+        return data;
     }
     catch (error) {
-        throw new Error("Error fetching ads: ", error);
+        console.error("Error registering user:", error.message);
         throw error;
     }
 };
